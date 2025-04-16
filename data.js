9136d6e1a635ac7d568f7d669b5a3b8e311a2f59
Johannes Schickling <schickling.j@gmail.com>
2025-04-16 17:27:54 +0200

    updated data

--- data.js
+++ data.js
@@ -8,25 +8,38 @@ export const data = LandscapeSchema.make({
 	Description: 'Client-centric local-first data layer for high-performance apps',
 	Website: 'https://livestore.dev',
 	Deployment: ['Self-hosted'],
-	License: 'MIT',
+	// License: 'MIT',
+	MaturityLevel: 'Beta',
+	// AuthIdentity: {AuthenticationMethod: {data: ['']}},
+	// TODO update link
+	GetStarted: 'https://livestore.dev',
+	// UserControlDataOwnership: ,
+	GitHub: 'https://github.com/livestore-dev/livestore',
+	DevelopmentWorkflowsDX: { DebuggingTools: { data: ['DevTools', 'Data Inspector', 'Network Inspector'] } },
 	AppTarget: {
 		LanguageSDK: { data: ['typescript'] },
+		ClientBundleSize: { data: '~500kb gzipped', comment: 'For web apps, LiveStore ships with a SQLite WASM build (~600kb / 300kb gzipped) and the library itself is around 500kb / 170kb gzipped.' },
+		FrameworkIntegrations: { data: ['React', 'React Native', 'Solid'] },
+		Platform: { data: ['Browser', 'Node', 'iOS', 'Android'] }
 	},
 	Networking: {
 		Topology: { data: 'Client-Server' },
+		Protocol: { data: ['WebSockets', 'HTTP', 'SSE'], comment: 'Given LiveStore supports arbitrary sync backends, any kind of network protocol can be used.' },
 	},
 	ServerSideData: {
-		PersistenceMechanism: { data: ['N/A'] },
-		DataModelParadigm: { data: 'Relational' },
+		PersistenceMechanism: { data: ['Cloudflare D1', 'Postgres', 'SQLite', 'Custom'], comment: 'Almost anything works as long as it can persist an eventlog.' },
+		DataModelParadigm: { data: 'Eventlog' },
+		DataSize: { data: '10GB', comment: `Theoretically there isn't an upper limit to the eventlog size however at some point it might become impractical to sync the eventlog over the network.` }
 	},
 	ClientSideData: {
 		QueryAPI: { data: ['Async'] },
 		PersistenceMechanism: { data: ['IndexedDB', 'OPFS'] },
 		PersistenceFeatures: { data: 'Indexes' },
-		DataModel: { data: 'Document' },
+		DataModel: { data: 'Event Sourcing', comment: 'LiveStore follows the event sourcing pattern which separates writes into an eventlog and materializes state into a local SQLite database.' },
 		OfflineReads: { data: 'Full Support' },
 		OfflineWrites: { data: 'Local conflict resolution' },
-		DataSize: { data: 'Up to 5-10 MB per document' },
+		DataSize: { data: '<1GB', comment: 'The upper limit is mostly determined by the used browser or client device. SQLite stores data fairly efficiently, so you should rarely hit any limits in regards to size.' },
+		LocalRefreshLatency: { data: '~1ms', comment: 'LiveStore is designed to enable next-frame refresh latency by using an in-memory SQLite database with a signals-based reactivivity system.' }
 	},
 	SynchronizationStrategy: {
 		FullOrPartialReplication: { data: ['Full Replication'] },
